66d7917ee432face247c972276675730b6cfab25
86 <triaedz@gmail.com>
2014-06-05 00:24:56 +0900

    Initial Commit

---
+++ TRZSyncScrollNavBarTitle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TRZSyncScrollNavBarTitle
+//
+//  Created by yam on 2014/06/05.
+//  Copyright (c) 2014年 86. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
